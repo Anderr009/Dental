bb0ba67fb7bd2314a578d79e0fcbda229c1cd584
Ander <Anderson_ortiz05@hotmail.com>
2023-07-18 12:31:15 -0400

    cambios finales

--- API/serializers/EvaluacionCliente.py
+++ API/serializers/EvaluacionCliente.py
@@ -11,22 +11,30 @@ class evaluacionCliente(serializers.ModelSerializer):
     def get_evaluacion_set(self, cliente):
         evaluaciones = cliente.evaluacion_set.all()
         evaluaciones_data = EvaluacionSerializer(evaluaciones, many=True).data
+
         for evaluacion_data in evaluaciones_data:
             cod_eval = evaluacion_data['cod_ev']
+            eval_proced = Eval_Proced.objects.filter(cod_eval_id=cod_eval).first()
+            if eval_proced:
+                evaluacion_data['pago_total'] = eval_proced.pago_total
+                evaluacion_data['abono'] = eval_proced.abono 
+
             procedimientos = Procedimiento.objects.filter(eval_proced__cod_eval_id=cod_eval)
             procedimientos_data = ProcedSerializer(procedimientos, many=True).data
+
             for procedimiento_data in procedimientos_data:
                 if eval_proced:
                     procedimiento_data['pago_total'] = eval_proced.pago_total
                     procedimiento_data['abono'] = eval_proced.abono
-            evaluacion_data['procedimientos'] = procedimientos_data
+
             preguntas_evaluacion = Pregunta_Evaluacion.objects.filter(fk_ev_id=cod_eval)
             preguntas_data = PregEvSerializer(preguntas_evaluacion, many=True).data
-            eval_proced = Eval_Proced.objects.filter(cod_eval_id=cod_eval).first()
             evaluacion_data['preguntas_evaluacion'] = preguntas_data
+
+            evaluacion_data['procedimientos'] = procedimientos_data
+
         return evaluaciones_data
 
     class Meta:
         model = Cliente
         fields = ['id', 'cedula', 'nombres', 'direccion', 'sexo', 'edad', 'apellido', 'evaluacion_set']
-    
